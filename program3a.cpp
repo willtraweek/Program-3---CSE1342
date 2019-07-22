d0a8b8b1d768f2aa2da1039dc49b1a4423777a6e
Will Traweek <will.traweek@gmail.com>
2019-07-22 18:59:19 -0500

    Add Functions for Sections 3+4 of Part A

--- program3a.cpp
+++ program3a.cpp
@@ -7,22 +7,44 @@ using namespace std;
 int maximum(int[], int);
 void oddSwap(int * x, int * y);
 int * expand(int[], int);
+int * concatenate(int[],int,int[],int);
+void printArray(int*,int);
 
 int main(){
-	int test1[ARRAYSIZE];
+	int * test1 = new int[ARRAYSIZE];
 	test1[0] = 2;
 	test1[1] = 10;
 	test1[2] = -2;
 	test1[3] = -100;
 	test1[4] = 0;
 
-	cout << "Maximum Test: " << test1 << endl;
+	cout << "ARRAY:" << endl;
+	printArray(test1, ARRAYSIZE);
+
+	cout << endl << "Maximum Test: " << endl;
 	cout << maximum(test1, ARRAYSIZE) << endl;
 
 	cout << endl << "oddSwap test:" << endl;
-	cout << "Before: " << test1[0] << " " << test1[1] << endl;
+	cout << "Before oddSwap: " << test1[0] << " " << test1[1] << endl;
 	oddSwap(test1, (test1+1));
-	cout << "After: " << test1[0] << " " << test1[1] << endl;
+	cout << "After oddSwap: " << test1[0] << " " << test1[1] << endl << endl;
+
+	
+
+	cout << "Expand:\n" <<endl << "Before:\n";
+	printArray(test1,ARRAYSIZE);
+	int * test2 = expand(test1, ARRAYSIZE);
+	cout << endl << "After:\n";
+	printArray(test2, ARRAYSIZE*2);
+	cout << endl;
+
+	cout << "Concatenate:\nArray1:\n";
+	printArray(test1, ARRAYSIZE);
+	cout << "\nArray2:\n";
+	printArray(test2, ARRAYSIZE*2);
+	cout<<"\nAfter:\n";
+	int * test3 = concatenate(test1, ARRAYSIZE, test2, ARRAYSIZE*2);
+	printArray(test3, ARRAYSIZE*3);
 
     return 0;
 }
@@ -44,10 +66,31 @@ void oddSwap(int * x, int * y){
 }
 
 int * expand(int array[], int size){
-	int temp[(size*2)];
-
+	int * temp = new int[size*2];
 	for(int i = 0; i <size; i++){
 		*(temp+i) = *(array+i);
 	}
+	for(int i = size; i < size*2; i++){
+		*(temp+i) = -1;
+	}
 	return (temp);
+}
+
+int * concatenate(int * array1, int array1size, int * array2, int array2size){
+	int * temp = new int[array1size+array2size];
+	for(int i = 0; i < array1size+array2size; i++){
+		if(i < array1size){
+			*(temp+i) = *(array1+i);
+		}else{
+			*(temp+i) = *(array2+i-array1size);
+		}
+	}
+
+	return temp;
+}
+
+void printArray(int * array, int size){
+	for(int i = 0; i < size; i++){
+		cout << i << "\t" << array+i << "\t" << *(array+i) << endl;
+	}
 }
